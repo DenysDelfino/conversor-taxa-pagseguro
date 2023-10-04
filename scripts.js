c3749dff3677dd3401bd5d23616e9671d8c2c37d
DenysDelfino <denyslacerda1990@gmail.com>
2023-10-04 12:39:21 -0300

    Alterações para Débito e Crédito e formula das taxas

--- scripts.js
+++ scripts.js
@@ -1,29 +1,41 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
+ 
+
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".valor").value
     const currencyValueToConvert = document.querySelector(".entry-value")//Valor da venda - entrada
     const currencyValueConverted = document.querySelector(".exit-value")//Valor Final - 
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
+    const valor = inputCurrencyValue
+    const taxaDebito = 2.39
+    const valorFinal = valor * (1 - taxaDebito / 100)
+
+    const value = inputCurrencyValue
+    const taxaCredito = 4.99
+    const valorCredito = value * (1 - taxaCredito / 100)
+     
+
+    
+    const aVista = valorFinal
+    const aPrazo = valorCredito
 
 
-    if (currencySelect.value == "dolar") {
+    if (currencySelect.value == "debito") {
         //Se o select estiver selecionado o valor de dolar, entre aqui
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
-            currency: "USD"
-        }).format(inputCurrencyValue / dolarToday)
+            currency: "BRL"
+        }).format(aVista)
     }
 
-    if (currencySelect.value == "euro") {
+    if (currencySelect.value == "credito") {
         //Se o select estiver selecionado o valor de euro, enre aqui
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
-            currency: "EUR"
-        }).format(inputCurrencyValue / euroToday)
+            currency: "BRL"
+        }).format(aPrazo)
     }
 
 
@@ -33,18 +45,18 @@ function convertValues() {
     }).format(inputCurrencyValue) // Formatação do número para moeda corrente
 }
 
-function changeCurrency(){
+function changeCurrency() {
     const currencyName = document.getElementById("currency-name")
     const currencyImage = document.querySelector(".currency-img")
 
-    if(currencySelect.value == "dolar"){
-        currencyName.innerHTML = "Dólar americano"
+    if (currencySelect.value == "debito") {
+        currencyName.innerHTML = "Débito"
         currencyImage.src = "./assets/imagens-de-dinheiro-real-png-3.png"
     }
 
-    if(currencySelect.value == "euro"){
-        currencyName.innerHTML = "Euro"
-        currencyImage.src = "./assets/PagSeguro-Logo.png"
+    if (currencySelect.value == "credito") {
+        currencyName.innerHTML = "Crédito"
+        //currencyImage.src = "./assets/PagSeguro-Logo.png"
     }
 
     convertValues()
